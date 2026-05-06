ec035dc07f7fc0bb9c9cdfabbebed999c2be809f
34ERAT <philipkanuti@gmail.com>
2026-05-06 20:39:35 +0300

    press ctrl-q to quit refactor keyboard input

--- kilo.c
+++ kilo.c
@@ -1,9 +1,16 @@
+#include <cerrno>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+/* defines */
+#define CTRL_KEY(K) ((K) & 0x1f)
+/** data **/
+/*** init***/
+
+/*** terminal ***/
 struct termios orig_termios;
 void die(const char *s) {
   perror(s);
@@ -27,20 +34,28 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
     die("tcsetattr");
 }
+char editorReadkey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+  return c;
+}
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadkey();
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
 int main() {
   enableRawMode();
   while (1) {
-
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-      die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q')
-      break;
+    editorProcessKeypress();
   }
   return 0;
 }
