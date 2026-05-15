6356f1a5ef5b11b4b2ea0ee8e815b608da920555
34ERAT <philipkanuti@gmail.com>
2026-05-15 22:00:13 +0300

    Quit confirmation of the with unsaved changes

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@
 #define CTRL_KEY(K) ((K) & 0x1f)
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 enum editorKey {
   BACKSPACE = 127,
@@ -487,12 +488,20 @@ void editorMoveCursor(int key) {
     E.cx = rowlen;
 }
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
   int c = editorReadKey();
   switch (c) {
   case '\r':
     /* TODO: */
     break;
   case CTRL_KEY('q'):
+    if (E.dirty && quit_times > 0) {
+
+      editorSetStatusMessage("WARNING!!! File has unsaved changes .",
+                             "Press Ctrl-Q %d more times to quit", quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -542,6 +551,7 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+  quit_times = KILO_QUIT_TIMES;
 }
 /** init**/
 void initEditor() {
